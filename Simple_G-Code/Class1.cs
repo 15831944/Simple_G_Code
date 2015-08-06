741995e17d85de424f3af6ce38e0fcbbc3b964ff
Boxa <Boxa.shu@gmail.com>
2015-08-06 08:29:31 +0300

    https://github.com/BoxaShu/Simple_G_Code.git

--- Simple_G-Code/Class1.cs
+++ Simple_G-Code/Class1.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Xml.Serialization;
 using App = Autodesk.AutoCAD.ApplicationServices;
 using cad = Autodesk.AutoCAD.ApplicationServices.Application;
 using Db = Autodesk.AutoCAD.DatabaseServices;
@@ -15,6 +17,7 @@ using Rtm = Autodesk.AutoCAD.Runtime;
 
 namespace Simple_G_Code
 {
+            
     public class Commands
     {
 
@@ -23,27 +26,23 @@ namespace Simple_G_Code
         const double arcAngleSegm = 5; //угол сегмента при апроксимации кривой (5 градусов)
         const string path = "D:\\";  //Папка для сохранения файлов g-code
         const string Z_feed = "0";  //рабочий уровень инструмента
-        const string Z_seed = "1";  // уровень перемещения инструмента
+        const string Z_seed = "3";  // уровень перемещения инструмента
+        const int round = 3;  // уровень jrheuktybz
 
+        //public static Settings setting = new Settings();
 
         [Rtm.CommandMethod("Get_GCode_from_pline")]
         static public void Get_GCode_from_pline()
         {
+            //setting = setting.getParam();
+            //saveParam(setting);
+            
             // Получение текущего документа и базы данных
             App.Document acDoc = App.Application.DocumentManager.MdiActiveDocument;
             Db.Database acCurDb = acDoc.Database;
             Ed.Editor acEd = acDoc.Editor;
 
-
-            //Ed.PromptEntityOptions opt = new Ed.PromptEntityOptions("\nВыбери плинию:");
-            //Ed.PromptEntityResult rez = acEd.GetEntity(opt);
-            //if (rez.Status != Ed.PromptStatus.OK)
-            //    return;
-
-
-            //Db.TypedValue[] acTypValAr=new Db.TypedValue[1];
-            Db.TypedValue[] acTypValAr = new Db.TypedValue[1] { new Db.TypedValue(0, "LWPOLYLINE,POLYLINE") };
-            //acTypValAr.SetValue(new Db.TypedValue((int)Db.DxfCode.Start, "PLINE"), 0);
+            Db.TypedValue[] acTypValAr = new Db.TypedValue[1] { new Db.TypedValue(0, "LINE,LWPOLYLINE,POLYLINE") };
             Ed.SelectionFilter acSelFtr = new Ed.SelectionFilter(acTypValAr);
             Ed.PromptSelectionResult acSSPrompt = acDoc.Editor.GetSelection(acSelFtr);
             if (acSSPrompt.Status != Ed.PromptStatus.OK)
@@ -51,96 +50,145 @@ namespace Simple_G_Code
 
             Ed.SelectionSet acSSet = acSSPrompt.Value;
 
-
-           
-            //string podachaXY = " F2500";
-            //string podachaZ = " F200";
-            string gcode = "\nG0 X0 Y0 " + podachaXY;
+            string gcode = "";
+            gcode = gcode + "G1 Z" + Z_seed + podachaZ;
+            gcode = gcode + "\nG0 X0 Y0 " + podachaXY;
 
             bool down = true;
             // старт транзакции
             using (Db.Transaction acTrans = acCurDb.TransactionManager.StartOpenCloseTransaction())
             {
                 // Открытие таблицы Блоков для чтения
-                Db.BlockTable acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId, Db.OpenMode.ForRead) as Db.BlockTable;
+                Db.BlockTable acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId, 
+                    Db.OpenMode.ForRead) as Db.BlockTable;
 
                 // Открытие записи таблицы Блоков пространства Модели для записи
                 Db.BlockTableRecord acBlkTblRec = acTrans.GetObject(acBlkTbl[Db.BlockTableRecord.ModelSpace],
-                                                                                Db.OpenMode.ForWrite) as Db.BlockTableRecord;
+                    Db.OpenMode.ForWrite) as Db.BlockTableRecord;
 
-            
                 foreach (Ed.SelectedObject acSSObj in acSSet)
                 {
-
-                    Db.Polyline acPl = acTrans.GetObject(acSSObj.ObjectId, Db.OpenMode.ForRead) as Db.Polyline;
-                    
-                    
-                    for (int i = 0; i < acPl.NumberOfVertices; i++)
+                    if (acSSObj.ObjectId.ObjectClass.IsDerivedFrom(Rtm.RXClass.GetClass(typeof(Db.Polyline))))
                     {
+                            Db.Polyline acPl = acTrans.GetObject(acSSObj.ObjectId, Db.OpenMode.ForRead) as Db.Polyline;
+                            for (int i = 0; i < acPl.NumberOfVertices; i++)
+                            {
+                                Gem.Point2d p2d = acPl.GetPoint2dAt(i);
+                                string x = Math.Round(p2d.X, round).ToString().Replace(',', '.');
+                                string y = Math.Round(p2d.Y, round).ToString().Replace(',', '.');
 
-                        Gem.Point2d p2d = acPl.GetPoint2dAt(i);
-                        string x = Math.Round(p2d.X, 2).ToString().Replace(',', '.');
-                        string y = Math.Round(p2d.Y, 2).ToString().Replace(',', '.');
+                                gcode = gcode + "\nG0 X" + x + " Y" + y + podachaXY;
+                                //Db.DBPoint acP = new Db.DBPoint(new Gem.Point3d(p2d.X, p2d.Y, 0));
+                                //acP.SetDatabaseDefaults();
+                                //acBlkTblRec.AppendEntity(acP);
+                                //acTrans.AddNewlyCreatedDBObject(acP, true);
+                                if (down)
+                                {
+                                    gcode = gcode + "\nG1 Z" + Z_feed + podachaZ;
+                                    down = false;
+                                }
+                                switch (acPl.GetSegmentType(i))
+                                {
+                                    case Db.SegmentType.Arc:
 
-                        gcode = gcode + "\nG0 X" + x + " Y" + y + podachaXY;
+                                        Gem.CircularArc3d arc3D = acPl.GetArcSegmentAt(i);
+                                        double delParam = (double)0.1;
+                                        double angle_1 = arc3D.StartAngle * (180 / Math.PI);
+                                        double angle_2 = arc3D.EndAngle * (180 / Math.PI);
+                                        //Сигменты с сектором не больше 5 градусов
+                                        delParam = 1 / (Math.Abs(angle_2 - angle_1) / arcAngleSegm);
+                                        for (Double curParam = i + delParam; (curParam + delParam) < i + 1; curParam += delParam)
+                                        {
+                                            Gem.Point3d curPt = acPl.GetPointAtParameter(curParam);
+                                            x = Math.Round(curPt.X, round).ToString().Replace(',', '.');
+                                            y = Math.Round(curPt.Y, round).ToString().Replace(',', '.');
+                                            gcode = gcode + "\nG1 X" + x + " Y" + y + podachaXY;
+                                            //Db.DBPoint acPArc = new Db.DBPoint(curPt);
+                                            //acPArc.SetDatabaseDefaults();
+                                            //acBlkTblRec.AppendEntity(acPArc);
+                                            //acTrans.AddNewlyCreatedDBObject(acPArc, true);
+                                        }
+                                        break;
+                                    case Db.SegmentType.Line:
+                                        Gem.LineSegment3d line3D = acPl.GetLineSegmentAt(i);
+                                        break;
+                                }
+                            }
 
-                        // Создание отрезка начинающегося в 0,0 и заканчивающегося в 5,5
-                        Db.DBPoint acP = new Db.DBPoint(new Gem.Point3d(p2d.X,p2d.Y,0));
-                        acP.SetDatabaseDefaults();
-                        acBlkTblRec.AppendEntity(acP);
-                        acTrans.AddNewlyCreatedDBObject(acP, true);                   
+                            if (acPl.Closed)
+                            {
+                                Gem.Point2d p2d = acPl.GetPoint2dAt(0);
+                                string x = Math.Round(p2d.X, round).ToString().Replace(',', '.');
+                                string y = Math.Round(p2d.Y, round).ToString().Replace(',', '.');
+                                gcode = gcode + "\nG1 X" + x + " Y" + y + podachaXY;
+                            }
+
+                            gcode = gcode + "\nG1 Z" + Z_seed + podachaZ;
+                            down = true;
+                        } // Конец обработки полилиний
+                    
+                        if (acSSObj.ObjectId.ObjectClass.IsDerivedFrom(Rtm.RXClass.GetClass(typeof(Db.Line))))
+                        {
+                            Db.Line acL = acTrans.GetObject(acSSObj.ObjectId, Db.OpenMode.ForRead) as Db.Line;
+                            string x = Math.Round(acL.StartPoint.X, round).ToString().Replace(',', '.');
+                            string y = Math.Round(acL.StartPoint.Y, round).ToString().Replace(',', '.');
+                            gcode = gcode + "\nG0 X" + x + " Y" + y + podachaXY; // переместится к началу отрезка
+                            gcode = gcode + "\nG1 Z" + Z_feed + podachaZ; // опустить инструмент
+                            x = Math.Round(acL.EndPoint.X, round).ToString().Replace(',', '.');
+                            y = Math.Round(acL.EndPoint.Y, round).ToString().Replace(',', '.');
+                            gcode = gcode + "\nG1 X" + x + " Y" + y + podachaXY; // переместится к концу отрезка
+                            gcode = gcode + "\nG1 Z" + Z_seed + podachaZ; //поднять инструмент
+                        }
+
                         
-                        if (down)
-                        {
-                            gcode = gcode + "\nG1 Z" + Z_feed + podachaZ;
-                            down = false;
-                        }
+                    
+                    //if (acSSObj.ObjectId.ObjectClass.IsDerivedFrom(Rtm.RXClass.GetClass(typeof(Db.Arc))))
+                    //    {
+                    //        Db.Arc acL = acTrans.GetObject(acSSObj.ObjectId, Db.OpenMode.ForRead) as Db.Arc;
+                    //        Db.Curve acE = acTrans.GetObject(acSSObj.ObjectId, Db.OpenMode.ForRead) as Db.Curve;
+                        
+                    //        Gem.Point3d p2d = acL.StartPoint;
+                    //        string x = Math.Round(p2d.X, 2).ToString().Replace(',', '.');
+                    //        string y = Math.Round(p2d.Y, 2).ToString().Replace(',', '.');
+
+                    //        gcode = gcode + "\nG0 X" + x + " Y" + y + podachaXY;
+                    //        if (down)
+                    //        {
+                    //            gcode = gcode + "\nG1 Z" + Z_feed + podachaZ;
+                    //            down = false;
+                    //        }
+                    //        //Gem.CircularArc3d arc3D = acL.;
 
 
-                        switch (acPl.GetSegmentType(i))
-                        {
-                            case Db.SegmentType.Arc:
+                    //        double delParam = (double)0.1;
+                    //        double angle_1 = acL.StartAngle * (180 / Math.PI);
+                    //        double angle_2 = acL.EndAngle * (180 / Math.PI);
+                    //        //Сигменты с сектором не больше 5 градусов
+                    //        delParam = 1 / (Math.Abs(angle_2 - angle_1) / arcAngleSegm);
 
-                               Gem.CircularArc3d arc3D = acPl.GetArcSegmentAt(i);
-                               double delParam = (double)0.1;
-
-                                double angle_1 = arc3D.StartAngle * (180 / Math.PI);
-                                double angle_2 = arc3D.EndAngle * (180 / Math.PI);
-                                    //Сигменты с сектором не больше 5 градусов
-                                delParam = 1 / (Math.Abs(angle_2 - angle_1) / arcAngleSegm);
+                    //        for (Double curParam = delParam; (curParam + delParam) < 1; curParam += delParam)
+                    //        {
+                    //            //Gem.Point3d curPt = acL.GetPointAtParameter(curParam);
+                    //            Gem.Point3d curPt = acE.GetPointAtParameter(curParam);
 
 
-                                for (Double curParam = i + delParam; (curParam + delParam) < i + 1; curParam += delParam)
-                                    {
+                    //            Db.DBPoint acPArc = new Db.DBPoint(curPt);
+                    //            acPArc.SetDatabaseDefaults();
+                    //            acBlkTblRec.AppendEntity(acPArc);
+                    //            acTrans.AddNewlyCreatedDBObject(acPArc, true);
 
-                                        Gem.Point3d curPt = acPl.GetPointAtParameter(curParam);
-                                        x = Math.Round(curPt.X, 2).ToString().Replace(',', '.');
-                                        y = Math.Round(curPt.Y, 2).ToString().Replace(',', '.');
-
-                                        gcode = gcode + "\nG1 X" + x + " Y" + y + podachaXY;
-
-                                        Db.DBPoint acPArc = new Db.DBPoint(curPt);
-                                        acPArc.SetDatabaseDefaults();
-                                        acBlkTblRec.AppendEntity(acPArc);
-                                        acTrans.AddNewlyCreatedDBObject(acPArc, true);  
-                                    
-                                        //Gem.Point3d curPt =  acPl.GetPointAtParameter(curParam);
-                                    
-                                        //curParam += delParam;
-                                    }
-                                    break;
-                            case Db.SegmentType.Line:
-                                Gem.LineSegment3d line3D = acPl.GetLineSegmentAt(i);
-                                //acEd.WriteMessage("\nCenter: {0}, radius: {1}", arc3D.Center, arc3D.Radius);
-                                break;
-                        }
-
+                    //            x = Math.Round(curPt.X, 2).ToString().Replace(',', '.');
+                    //            y = Math.Round(curPt.Y, 2).ToString().Replace(',', '.');
+                    //            gcode = gcode + "\nG1 X" + x + " Y" + y + podachaXY;
+                    //        }
+                    //        gcode = gcode + "\nG1 Z" + Z_seed + podachaZ;
+                    //    }
 
                     }
 
-                    gcode = gcode + "\nG1 Z" + Z_seed + podachaZ;
-                    down = true;
-                }
+
+
+
 
 
                 acTrans.Commit();
@@ -162,5 +210,6 @@ namespace Simple_G_Code
                 file.Write(gcode);
             }
         }
+
     }
 }
---
+++ Simple_G-Code/Settings.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml.Serialization;
+using Db = Autodesk.AutoCAD.DatabaseServices;
+
+
+namespace Simple_G_Code
+{
+    
+        //const string podachaXY = " F1000"; //Скорость подачи для осей XY
+        //const string podachaZ = " F200"; //скорость подачи для оси Z
+        //const double arcAngleSegm = 5; //угол сегмента при апроксимации кривой (5 градусов)
+        //const string path = "D:\\";  //Папка для сохранения файлов g-code
+        //const string Z_feed = "0";  //рабочий уровень инструмента
+        //const string Z_seed = "3";  // уровень перемещения инструмента
+        //const int round = 3;  // уровень jrheuktybz  
+    
+    
+    public class Settings
+    {
+        private Db.LineWeight _weight = Db.LineWeight.LineWeight009;
+        private int _colorInd = 10;
+        private string _layerName = "xline";
+
+        public Settings()
+        {
+        }
+
+        public Settings(int colorInd, Db.LineWeight weight, string layerName)
+        {
+            this.colorInd = colorInd;
+            this.weight = weight;
+            this.layerName = layerName;
+        }
+
+
+
+        public int colorInd
+        {
+            get { return _colorInd; }
+            set { _colorInd = value; }
+        }
+        public Db.LineWeight weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+
+        public string layerName
+        {
+            get { return _layerName; }
+            set { _layerName = value; }
+        }
+
+
+
+        public Settings getParam()
+        {
+
+            string path = Assembly.GetExecutingAssembly().Location;
+            FileInfo fi1 = new FileInfo(path);
+            path = fi1.Directory.ToString() + "\\MySettings.xml";
+            fi1 = null;
+
+            Settings myObject = default(Settings);
+            if (System.IO.File.Exists(path))
+            {
+                try
+                {
+                    XmlSerializer mySerializer = new XmlSerializer(typeof(Settings));
+                    using (FileStream myFileStream = new FileStream(path, FileMode.Open))
+                    {
+                        myObject = (Settings)mySerializer.Deserialize(myFileStream);
+                    }
+
+                }
+                catch (System.InvalidOperationException ex)
+                {
+                    System.IO.File.Delete(path);
+                    myObject = new Settings(256, Db.LineWeight.LineWeight009, "xline");
+                }
+
+            }
+            else
+            {
+                myObject = new Settings(256, Db.LineWeight.LineWeight009, "xline");
+            }
+            return myObject;
+        }
+
+        public void saveParam(Settings Setting)
+        {
+            string path = Assembly.GetExecutingAssembly().Location;
+            FileInfo fi1 = new FileInfo(path);
+            path = fi1.Directory.ToString() + "\\MySettings.xml";
+            fi1 = null;
+
+            XmlSerializer ser = new XmlSerializer(typeof(Settings));
+            using (TextWriter writer = new StreamWriter(path, false))
+            {
+                ser.Serialize(writer, Setting);
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
